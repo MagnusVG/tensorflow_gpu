9ba9ec29e15db09ff8e2c389c30e117e5c306e0a
MagnusVG <54357342+MagnusVG@users.noreply.github.com>
2022-11-01 19:28:08 +0100

    Add files via upload

--- preprocess.sh
+++ preprocess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --gres=gpu:0
-#SBATCH --partition=cpu36 
-#SBATCH --time=12:00:00
+#SBATCH --partition=cpu20 
+#SBATCH --time=168:00:00
 #SBATCH --job-name=preprocess_area
 #SBATCH --output=preprocess_area_part1.out
  
@@ -9,4 +9,4 @@
 uenv miniconda-python39
 conda activate pandas_env
 # Run the Python script that uses the GPU
-python -u preprocess_data.py
+python -u preprocess_data.py --part=1 --neighbors=2
